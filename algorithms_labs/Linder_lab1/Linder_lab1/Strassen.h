a4d9622614dc077349a3961f9e386ebeb7618d9b
onisalex1@gmail.com <onisalex1@gmail.com>
2022-10-23 16:06:41 +0800

    changed the implementation for different matrices

--- algorithms_labs/Linder_lab1/Linder_lab1/Strassen.h
+++ algorithms_labs/Linder_lab1/Linder_lab1/Strassen.h
@@ -59,67 +59,96 @@ Matrix* strassen_multiply(Matrix* matrix1, Matrix* matrix2, const int& n) {
 		return matrix_multiply(matrix1,matrix2,n);
 	}
 
-	Matrix* a00 = new Matrix(n / 2);
-	Matrix* a01 = new Matrix(n / 2);
-	Matrix* a10 = new Matrix(n / 2);
-	Matrix* a11 = new Matrix(n / 2);
+	const int& new_size = n/2 + n%2;
 
-	for (int i = 0; i < n / 2; i++) {
-		for (int j = 0; j < n / 2; j++) {
+
+
+	Matrix* a00 = new Matrix(new_size);
+	Matrix* a01 = new Matrix(new_size);
+	Matrix* a10 = new Matrix(new_size);
+	Matrix* a11 = new Matrix(new_size);
+
+	for (int i = 0; i < new_size; i++) {
+		for (int j = 0; j < new_size; j++) {
 			a00->matrix[i][j] = matrix1->matrix[i][j];
-			a01->matrix[i][j] = matrix1->matrix[i][j + n / 2];
-			a10->matrix[i][j] = matrix1->matrix[i + n / 2][j];
-			a11->matrix[i][j] = matrix1->matrix[i + n/2][j + n / 2];
+			if (j + new_size < n)
+				a01->matrix[i][j] = matrix1->matrix[i][j + new_size];
+			else
+				a01->matrix[i][j] = 0;
+
+			if (i + new_size < n)
+				a10->matrix[i][j] = matrix1->matrix[i + new_size][j];
+			else
+				a10->matrix[i][j] = 0;
+
+			if (i + new_size < n && j + new_size < n)
+				a11->matrix[i][j] = matrix1->matrix[i + new_size][j + new_size];
+			else
+				a11->matrix[i][j] = 0;
 		}
 	}
 
-	Matrix* b00 = new Matrix(n / 2);
-	Matrix* b01 = new Matrix(n / 2);
-	Matrix* b10 = new Matrix(n / 2);
-	Matrix* b11 = new Matrix(n / 2);
+	Matrix* b00 = new Matrix(new_size);
+	Matrix* b01 = new Matrix(new_size);
+	Matrix* b10 = new Matrix(new_size);
+	Matrix* b11 = new Matrix(new_size);
 
-	for (int i = 0; i < n / 2; i++) {
-		for (int j = 0; j < n / 2; j++) {
+	for (int i = 0; i < new_size; i++) {
+		for (int j = 0; j < new_size; j++) {
 			b00->matrix[i][j] = matrix2->matrix[i][j];
-			b01->matrix[i][j] = matrix2->matrix[i][j + n / 2];
-			b10->matrix[i][j] = matrix2->matrix[i + n / 2][j];
-			b11->matrix[i][j] = matrix2->matrix[i + n/2][j + n / 2];
+			if (j + new_size < n)
+				b01->matrix[i][j] = matrix2->matrix[i][j + new_size];
+			else
+				b01->matrix[i][j] = 0;
+
+			if (i + new_size < n)
+				b10->matrix[i][j] = matrix2->matrix[i + new_size][j];
+			else
+				b10->matrix[i][j] = 0;
+
+			if (i + new_size < n && j + new_size < n)
+				b11->matrix[i][j] = matrix2->matrix[i + new_size][j + new_size];
+			else
+				b11->matrix[i][j] = 0;
 		}
 	}
 
-	Matrix* s1 = matrix_subtraction(b01, b11, n / 2);
-	Matrix* s2 = matrix_addition(a00, a01, n / 2);
-	Matrix* s3 = matrix_addition(a10, a11, n / 2);
-	Matrix* s4 = matrix_subtraction(b10, b00, n / 2);
-	Matrix* s5 = matrix_addition(a00, a11, n / 2);
-	Matrix* s6 = matrix_addition(b00, b11, n / 2);
-	Matrix* s7 = matrix_subtraction(a01, a11, n / 2);
-	Matrix* s8 = matrix_addition(b10, b11, n / 2);
-	Matrix* s9 = matrix_subtraction(a00, a10, n / 2);
-	Matrix* s10 = matrix_addition(b00, b01, n / 2);
+	Matrix* s1 = matrix_subtraction(b01, b11, new_size);
+	Matrix* s2 = matrix_addition(a00, a01, new_size);
+	Matrix* s3 = matrix_addition(a10, a11, new_size);
+	Matrix* s4 = matrix_subtraction(b10, b00, new_size);
+	Matrix* s5 = matrix_addition(a00, a11, new_size);
+	Matrix* s6 = matrix_addition(b00, b11, new_size);
+	Matrix* s7 = matrix_subtraction(a01, a11, new_size);
+	Matrix* s8 = matrix_addition(b10, b11, new_size);
+	Matrix* s9 = matrix_subtraction(a00, a10, new_size);
+	Matrix* s10 = matrix_addition(b00, b01, new_size);
 
-	Matrix* p1 = strassen_multiply(a00, s1, n / 2);
-	Matrix* p2 = strassen_multiply(s2, b11, n / 2);
-	Matrix* p3 = strassen_multiply(s3, b00, n / 2);
-	Matrix* p4 = strassen_multiply(a11, s4, n / 2);
-	Matrix* p5 = strassen_multiply(s5, s6, n / 2);
-	Matrix* p6 = strassen_multiply(s7, s8, n / 2);
-	Matrix* p7 = strassen_multiply(s9, s10, n / 2);
+	Matrix* p1 = strassen_multiply(a00, s1, new_size);
+	Matrix* p2 = strassen_multiply(s2, b11, new_size);
+	Matrix* p3 = strassen_multiply(s3, b00, new_size);
+	Matrix* p4 = strassen_multiply(a11, s4, new_size);
+	Matrix* p5 = strassen_multiply(s5, s6, new_size);
+	Matrix* p6 = strassen_multiply(s7, s8, new_size);
+	Matrix* p7 = strassen_multiply(s9, s10, new_size);
 
-	Matrix* c1 = matrix_addition(matrix_subtraction(matrix_addition(p5,p4,n/2),p2,n/2),p6,n/2);
-	Matrix* c2 = matrix_addition(p1,p2,n/2);
-	Matrix* c3 = matrix_addition(p3, p4, n / 2);
-	Matrix* c4 = matrix_subtraction(matrix_subtraction(matrix_addition(p5, p1, n / 2), p3, n / 2), p7, n / 2);
+	Matrix* c1 = matrix_addition(matrix_subtraction(matrix_addition(p5,p4, new_size),p2, new_size),p6, new_size);
+	Matrix* c2 = matrix_addition(p1,p2, new_size);
+	Matrix* c3 = matrix_addition(p3, p4, new_size);
+	Matrix* c4 = matrix_subtraction(matrix_subtraction(matrix_addition(p5, p1, new_size), p3, new_size), p7, new_size);
 
 
 	Matrix* result = new Matrix(n);
 
-	for (int i = 0; i < n/2; i++) {
-		for (int j = 0; j < n/2; j++) {
+	for (int i = 0; i < new_size; i++) {
+		for (int j = 0; j < new_size; j++) {
 			result->matrix[i][j] = c1->matrix[i][j];
-			result->matrix[i][j + n / 2] = c2->matrix[i][j];
-			result->matrix[i + n / 2][j] = c3->matrix[i][j];
-			result->matrix[i + n / 2][j + n / 2] = c4->matrix[i][j];
+			if(j+new_size<n)
+				result->matrix[i][j + new_size] = c2->matrix[i][j];
+			if(i+new_size<n)
+				result->matrix[i + new_size][j] = c3->matrix[i][j];
+			if(j + new_size < n && i + new_size < n)
+				result->matrix[i + new_size][j + new_size] = c4->matrix[i][j];
 
 		}
 	}
